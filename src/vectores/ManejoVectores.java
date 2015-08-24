0edf7adbcebdd341a68ef9e90c95b1cf3f50e317
Luis Astudillo <luisonboard@gmail.com>
2015-08-24 09:53:12 -0500

    Se creó la clase Vector y ManejoVectores Se agregó la función vector resultante

---
+++ src/vectores/ManejoVectores.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vectores;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author User
+ */
+public  class ManejoVectores {
+    
+    /**
+     * Retorna el vector resultante de una suma de vectores
+     * @param v1 El primer vector de la suma
+     * @param v2 El último vector agregado a la suma
+     * @return
+     */
+    public static Vector vectorResultante(Vector v1, Vector v2){
+        Vector resultante = new Vector();
+        Point pInicial = v1.getPuntoInicial();
+        Point pFinal = v2.getPuntoFinal();
+        return resultante;        
+    }
+    
+}
