f61dcff0068be2c0fe4be6e47660e5d8aa40cc40
Luis Astudillo <luisonboard@gmail.com>
2015-08-24 21:18:18 -0500

    Se creó la clase Vector y ManejoVectores Se agregó la función vector resultante

---
+++ src/vectores/Vector.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package vectores;
+
+import java.awt.Point;
+
+/**
+ *
+ * @author User
+ */
+public class Vector {
+    
+    private int id;
+    private Point puntoInicial, puntoFinal;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Point getPuntoInicial() {
+        return puntoInicial;
+    }
+
+    public void setPuntoInicial(Point puntoInicial) {
+        this.puntoInicial = puntoInicial;
+    }
+
+    public Point getPuntoFinal() {
+        return puntoFinal;
+    }
+
+    public void setPuntoFinal(Point puntoFinal) {
+        this.puntoFinal = puntoFinal;
+    }
+    
+    
+    
+}
