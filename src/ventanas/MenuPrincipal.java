bf9348f4661a3035a91da69ed1c0a7da77f70102
HP USER <HP USER@HPUSER-HP>
2015-08-31 17:38:37 -0500

    Ya está terminada la tercera ventana  (MenuPrinicipal)

--- src/ventanas/MenuPrincipal.java
+++ src/ventanas/MenuPrincipal.java
@@ -38,34 +38,49 @@ public class MenuPrincipal extends JFrame{
         
         menu = new JLabel();
         menu.setText("MENÚ PRINCIPAL");
-        menu.setBounds(250, 50, 390, 40);
-        menu.setFont(new Font("Berlin Sans FB Demi", Font.BOLD, 40)); //Tipo y tamaño de letra
-	menu.setForeground(Color.yellow); //Pone color a las letras 
+        menu.setBounds(280, 50, 390, 40);
+        menu.setFont(new Font("Berlin Sans FB Demi", Font.BOLD, 30)); //Tipo y tamaño de letra
+	menu.setForeground(Color.BLUE); //Pone color a las letras 
         panelFondo.add(menu);
         
         btnTeoria = new JButton();
-        btnTeoria.setBounds(300, 100, 190, 40);
+        btnTeoria.setBounds(260, 140, 280, 40);
         btnTeoria.setText("FUNDAMENTOS TEÓRICOS");
+        btnTeoria.setFont(new Font("Sakkal Majalla", Font.BOLD, 24));
+        btnTeoria.setBorderPainted(false); //Desaparece el borde del botón
+        btnTeoria.setContentAreaFilled(false);
         panelFondo.add(btnTeoria);
         
         btnEjerResueltos = new JButton();
-        btnEjerResueltos.setBounds(300, 160, 190, 40);
+        btnEjerResueltos.setBounds(251, 200, 270, 40);
         btnEjerResueltos.setText("EJERCICIOS RESUELTOS");
+        btnEjerResueltos.setFont(new Font("Sakkal Majalla", Font.BOLD, 24));
+        btnEjerResueltos.setBorderPainted(false); //Desaparece el borde del botón
+        btnEjerResueltos.setContentAreaFilled(false);
         panelFondo.add(btnEjerResueltos);
         
         btnEjerPropuestos = new JButton();
-        btnEjerPropuestos.setBounds(300, 220, 190, 40);
+        btnEjerPropuestos.setBounds(256, 260, 280, 40);
         btnEjerPropuestos.setText("EJERCICIOS PROPUESTOS");
+        btnEjerPropuestos.setFont(new Font("Sakkal Majalla", Font.BOLD, 24));
+        btnEjerPropuestos.setBorderPainted(false); //Desaparece el borde del botón
+        btnEjerPropuestos.setContentAreaFilled(false);
         panelFondo.add(btnEjerPropuestos);
         
         btnSumaVectores = new JButton();
-        btnSumaVectores.setBounds(300, 280, 190, 40);
+        btnSumaVectores.setBounds(262, 320, 230, 40);
         btnSumaVectores.setText("SUMA DE VECTORES");
+        btnSumaVectores.setFont(new Font("Sakkal Majalla", Font.BOLD, 24));
+        btnSumaVectores.setBorderPainted(false); //Desaparece el borde del botón
+        btnSumaVectores.setContentAreaFilled(false);
         panelFondo.add(btnSumaVectores);
         
         btnReportesUso = new JButton();
-        btnReportesUso.setBounds(300, 340, 190, 40);
-        btnReportesUso.setText("SUMA DE VECTORES");
+        btnReportesUso.setBounds(256, 380, 230, 40);
+        btnReportesUso.setText("REPORTES DE USO");
+        btnReportesUso.setFont(new Font("Sakkal Majalla", Font.BOLD, 24));
+        btnReportesUso.setBorderPainted(false); //Desaparece el borde del botón
+        btnReportesUso.setContentAreaFilled(false);
         panelFondo.add(btnReportesUso);
         
         btnRegresar = new JButton();
